19758074571e7f183039e536189bb97f90cc6802
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-11-29 14:37:21 -0500

    Add edits to plant-id.html

---
+++ assets/js/apps.js
@@ -0,0 +1,23 @@
+var landing = angular.module("landing-pg", ["ngRoute"]);
+ console.log("working");
+landing.config(function($routeProvider){
+  console.log("and now");
+  $routeProvider
+    .when("/plant-id",{
+      templateUrl: "views/plant-id.html"
+
+    })
+    // .when("/projects",{
+    //   templateUrl: "views/projects.html"
+    //
+    // })
+    // .when("/resume",{
+    //   templateUrl: "views/resume.html"
+    // })
+    .otherwise({
+      redirectTo: "/home"
+
+    });
+
+});
+console.log("and again");
