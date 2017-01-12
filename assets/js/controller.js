0df3a63d3519489dba77e66a399a9c11fc63ea2e
Rhonda Goolsby <rlgoolsby@gmail.com>
2017-01-12 15:39:26 -0500

    Adding panel controller for nav-tabs on plants.html

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,21 +1,41 @@
 (function(){
 var app = angular.module('info', [ ]);
 
+
+// controller for the plant names //
 app.controller('InfoController', function(){
   this.id=plants;
+  console.log ('info working');
 });
 
 var plants = [{
   botanicalName: "Iris siberia",
   commonName: 'Siberian Iris',
-  description: "...",
+  image:
   wishList: true;
 },
 {
   botanicalName: "Crptomeria globosa nana",
   commonName: 'Dwarf Japanese Cedar',
-  description: "...",
+  image:
   wishList: true;
 }
 ]
-})();
+
+// controller for nav-tabs: Descriptions and Specifications //
+app.controller('PanelController', function(){
+  this.tab = 1;
+ console.log ("panel working");
+
+  this.selectTab = function(setTab) {
+     this.tab = setTab;
+   };
+});
+
+
+
+
+
+
+
+ })();
