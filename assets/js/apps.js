9b69a5d420b995e8ac2daa72d65c3f55dae3c179
Rhonda Goolsby <rlgoolsby@gmail.com>
2020-06-11 17:00:17 -0400

    move ng-view to different location on home page.

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -4,18 +4,18 @@ var app = angular.module("plants", ["ngRoute"]);
 app.config(function($routeProvider){
   console.log("and now");
   $routeProvider.
-    .when("#/",{
+    .when("/",{
       templateUrl: "home.html"
     })
-    .when("#/conditions",{
+    .when("/conditions",{
       templateUrl: "views/conditions.html",
       controller: "conditionsController"
     })
-    .when("#/local-nurseries",{
+    .when("/local-nurseries",{
       templateUrl: "views/local-nurseries.html"
     })
     .otherwise({
-      redirectTo: "#/"
+      redirectTo: "/"
     });
 });
 console.log("and again");
